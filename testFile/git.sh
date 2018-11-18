7f7dc799406338a54012649064551dcfc00ccdb8
ian841001 <ian841001@gmail.com>
2018-11-18 17:08:34 +0800

    init

--- testFile/git.sh
+++
@@ -1,6 +0,0 @@
-git --git-dir=/root/testFile/.git/ pull
-rm -r /root/testFile/dir/*
-cp -r /root/testFile/testFile/* /root/testFile/dir/
-git --git-dir=/root/testFile/.git/ add -A /root/testFile/
-git --git-dir=/root/testFile/.git/ commit -m init
-git --git-dir=/root/testFile/.git/ push
